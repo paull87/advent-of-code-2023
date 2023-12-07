4654ce82c97bace6cf8d8d58cde706ba9f98c7a5
PaulLucas <paul.lucas@leit.ltd>
2023-12-07 15:53:17 +0000

    Day 7

---
+++ day_7/day_7_part2.py
@@ -0,0 +1,112 @@
+
+input_file = 'input.txt'
+
+CARD_ORDERS = {
+    '5OK': 'A',
+    '4OK': 'B',
+    'FH': 'C',
+    '3OK': 'D',
+    '2P': 'E',
+    '1P': 'F',
+    'A': 'G',
+    'K': 'H',
+    'Q': 'I',
+    'T': 'K',
+    '9': 'L',
+    '8': 'M',
+    '7': 'N',
+    '6': 'O',
+    '5': 'P',
+    '4': 'Q',
+    '3': 'R',
+    '2': 'S',
+    'J': 'Z',
+}
+
+with open(input_file) as file:
+    input_data = file.readlines()
+
+
+def parse_data(text):
+    games = [
+        (x.split()[0].strip(), int(x.split()[1].strip())) for x in text
+    ]
+    return games
+
+
+def score_hand(hand):
+    # five of a kind
+    score = ''
+    unique_hand = list(set(hand))
+    j_less_hand = hand.replace('J', '')
+    jokers = len(hand) - len(j_less_hand)
+    # import pdb; pdb.set_trace()
+    if len(unique_hand) == 1:
+        score += CARD_ORDERS['5OK']
+    if len(unique_hand) == 2:
+        # four of kind
+        if len(hand.replace(unique_hand[0], '')) == 1 or len(hand.replace(unique_hand[1], '')) == 1:
+            score += CARD_ORDERS['4OK']
+        # full house
+        elif len(hand.replace(unique_hand[0], '')) in (2, 3) and len(hand.replace(unique_hand[1], '')) in (2, 3):
+            score += CARD_ORDERS['FH']
+    if len(unique_hand) == 3:
+        # 3 of kind
+        if len(hand.replace(unique_hand[0], '')) == 2 or len(hand.replace(unique_hand[1], '')) == 2 or len(hand.replace(unique_hand[2], '')) == 2:
+            score += CARD_ORDERS['3OK']
+        # 2 pair
+        else:
+            score += CARD_ORDERS['2P']
+    # pair
+    if len(unique_hand) == 4:
+        score += CARD_ORDERS['1P']
+
+    # cater for jokers and override score
+    if jokers > 0:
+        if jokers == 4:
+            score = CARD_ORDERS['5OK']
+        if jokers == 3:
+            if len(unique_hand) == 2:
+                score = CARD_ORDERS['5OK']
+            elif len(unique_hand) == 3:
+                score = CARD_ORDERS['4OK']
+        if jokers == 2:
+            if score == CARD_ORDERS['FH']:
+                score = CARD_ORDERS['5OK']
+            elif score == CARD_ORDERS['3OK']:
+                pass
+            elif score == CARD_ORDERS['2P']:
+                score = CARD_ORDERS['4OK']
+            elif score == CARD_ORDERS['1P']:
+                score = CARD_ORDERS['3OK']
+        if jokers == 1:
+            if score == CARD_ORDERS['4OK']:
+                score = CARD_ORDERS['5OK']
+            elif score == CARD_ORDERS['3OK']:
+                score = CARD_ORDERS['4OK']
+            elif score == CARD_ORDERS['2P']:
+                score = CARD_ORDERS['FH']
+            elif score == CARD_ORDERS['1P']:
+                score = CARD_ORDERS['3OK']
+            else:
+                score = CARD_ORDERS['1P']
+
+    for card in hand:
+        score += CARD_ORDERS[card]
+
+    return score
+
+
+def main():
+    games = parse_data(input_data)
+    total_winnings = 0
+    for rank, hand in enumerate(sorted(games, key=lambda x: score_hand(x[0]), reverse=True)):
+        winnings = (rank + 1) * hand[1]
+        total_winnings += winnings
+        print(rank + 1, hand[0], hand[1], winnings)
+        # print(hand[0], score_hand(hand))
+    print(total_winnings)
+
+
+if __name__ == '__main__':
+    main()
